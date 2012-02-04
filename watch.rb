c51ce39a9fa5c4cbc713b24f8eb1abd270cca091
Adam Mulligan <adammulligan@gmail.com>
2012-02-04 23:53:17 +0000

    Updated README; using clamp to parse CLI options

--- watch.rb
+++ watch.rb
@@ -2,31 +2,44 @@
 
 require 'fileutils'
 require 'net/sftp'
+require 'clamp'
 
-shots_dir = "#{ENV["HOME"]}/inbox/_screenshots"
-trans_dir = shots_dir+"/t"
+class ShoulderSurfer < Clamp::Command
+  option ["-d", "--shots_dir"], "d", "screenshots directory", :default => Dir.getwd
+  option ["-r", "--remote"], "user@host", "Remote user and host"
+  option ["-D", "--remote_dir"], "D", "Remote directory"
 
-remote_dir  = "/var/www/cyanoryx.com/public_html/files/s"
-remote_user = 'adam'
-remote_addr = 'cyanoryx.com'
-
-Dir.chdir(shots_dir)
-
-images = Dir.glob('*.{jpg,png,gif}', File::FNM_CASEFOLD)
-
-images.each do |file|
-  file_loc = "#{trans_dir}/#{file}"
-
-  begin 
-    Net::SFTP.start(remote_addr, remote_user) do |sftp|
-      sftp.upload!(file, "#{remote_dir}/#{file}")
+  def execute
+    trans_dir = shots_dir+"/t"
+    if !File.directory?(trans_dir)
+      Dir.mkdir(trans_dir)      
     end
 
-    # Move files that have been transferred
-    # In the future we'll probably just make an index and check for
-    # new files?
-    FileUtils.mv(file, "#{trans_dir}/#{file}")
-  rescue Net::SFTP::Exception
-    puts 'error'
+    remote_det = remote.split("@")
+    remote_user = remote_det[0]
+    remote_addr = remote_det[1]
+
+    Dir.chdir(shots_dir)
+
+    images = Dir.glob('*.{jpg,png,gif}', File::FNM_CASEFOLD)
+
+    images.each do |file|
+      file_loc = "#{trans_dir}/#{file}"
+
+      begin 
+        Net::SFTP.start(remote_addr, remote_user) do |sftp|
+          sftp.upload!(file, "#{remote_dir}/#{file}")
+        end
+
+        # Move files that have been transferred
+        # In the future we'll probably just make an index and check for
+        # new files?
+        FileUtils.mv(file, "#{trans_dir}/#{file}")
+      rescue Net::SFTP::Exception
+        puts 'error'
+      end
+    end
   end
 end
+
+ShoulderSurfer.run

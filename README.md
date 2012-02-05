c7c91429a150fa7747fc4e6ac05d17986fc76456
Adam Mulligan <adammulligan@gmail.com>
2012-02-05 10:38:38 +0000

    Made parameters a bit more pleasant (and used some common sense)

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Quick ruby util that uploads local screenshots in a directory to an sftp server.
 # Usage
 
 Combine with unix `watch` to automatically upload screenshots.
+Assumes you're using public key crypto; add a :password argument to SFTP.start() if needed.
 
 `$ watch -n time ./watch.rb -d screenshot_dir -D remote_dir -r remote_user@host`
--- watch.rb
+++ watch.rb
@@ -5,21 +5,22 @@ require 'net/sftp'
 require 'clamp'
 
 class ShoulderSurfer < Clamp::Command
-  option ["-d", "--shots_dir"], "d", "screenshots directory", :default => Dir.getwd
-  option ["-r", "--remote"], "user@host", "Remote user and host"
-  option ["-D", "--remote_dir"], "D", "Remote directory"
+  option "--quiet", :flag, "Quite mode - no output"
+  
+  parameter "LOCAL_DIR", "Local screenshots directory"
+  parameter "REMOTE", "Remote host and location - user@host:directory"
+
+  def remote=(remote)
+    @remote_user, @remote_host = (@remote_host, @remote_dir = remote.split(":")).first.split("@")
+  end
 
   def execute
-    trans_dir = shots_dir+"/t"
+    trans_dir = local_dir+"/t"
     if !File.directory?(trans_dir)
       Dir.mkdir(trans_dir)      
     end
 
-    remote_det = remote.split("@")
-    remote_user = remote_det[0]
-    remote_addr = remote_det[1]
-
-    Dir.chdir(shots_dir)
+    Dir.chdir(local_dir)
 
     images = Dir.glob('*.{jpg,png,gif}', File::FNM_CASEFOLD)
 
@@ -27,16 +28,18 @@ class ShoulderSurfer < Clamp::Command
       file_loc = "#{trans_dir}/#{file}"
 
       begin 
-        Net::SFTP.start(remote_addr, remote_user) do |sftp|
-          sftp.upload!(file, "#{remote_dir}/#{file}")
+        Net::SFTP.start(@remote_host, @remote_user) do |sftp|
+          print "Starting transfer - #{file}..." if not quiet?
+          sftp.upload!(file, "#{@remote_dir}/#{file}")
         end
+        puts 'Done' if not quiet?
 
         # Move files that have been transferred
         # In the future we'll probably just make an index and check for
         # new files?
         FileUtils.mv(file, "#{trans_dir}/#{file}")
       rescue Net::SFTP::Exception
-        puts 'error'
+        puts 'Could not connect to remote host'
       end
     end
   end
